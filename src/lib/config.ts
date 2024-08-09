ae358c6b9e391930f9033e860a975441b8e980e4
Antonio S. Dromundo <sebastiandromundo@outlook.com>
2024-08-08 19:17:37 -0600

    Updated trailing for Static Serving

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,6 +3,6 @@ import { dev } from '$app/environment'
 export const title = 'Laboratorio Remoto'
 export const description = 'Laboratorio Remoto - Análisis de Sistemas y Señales'
 export const url = dev ? 'http://localhost' : 'https://asdrome.eastus2.cloudapp.azure.com/'
-export const pocketbaseURL = dev ? "http://localhost:8090" : url + "pb"
+export const pocketbaseURL = dev ? "http://localhost:8090" : url
 export const streamURL = url + '/stream/api/whep'
 export const streamKey = 'RemoteLabStream'
